cd57510ccf0b70ce362678a30bff1f8c5b42211a
Ayushi Mittal <ayushimi@usc.edu>
2022-09-27 23:31:30 -0700

    table select call working

--- app.js
+++ app.js
@@ -1,9 +1,30 @@
 var express = require('express');
 var app = express();
+
+var pg = require('pg');
+
+var conString = "postgres://postgres:401db@localhost:5432/401_db";
+
+async function select_table(name) {
+  var client = new pg.Client(conString);
+  await client.connect();
+  const result = await client.query('SELECT * from ' + name);
+  console.log(result.rows[0]);
+  client.end();
+  return result.rows[0];
+};
+
 app.get('/', function (req, res) {
   res.send('Hello World!');
 });
 app.listen(process.env.PORT || 3000, function () {
   console.log('Example app listening on port 3000!');
 });
+
+app.get('/select', async function (req, res) {
+  var table_name = req.query.table_name;
+  var result = await select_table("administrator_info");
+  res.send(result);
+});
+
 
--- db.js
+++ db.js
@@ -2,26 +2,26 @@ var pg = require('pg');
 
 var conString = "postgres://postgres:401db@localhost:5432/401_db";
 
-var client = new pg.Client(conString);
-client.connect(function(err) {
-  if(err) {
-    return console.error('could not connect to postgres', err);
-  }
-  client.query('SELECT * from administrator_info', function(err, result) {
-    if(err) {
-      return console.error('error running query', err);
-    }
-    console.log(result.rows[0]);
-    client.end();
-  });
-}); 
 
 function select_table(name) {
-    client.query('SELECT * from ' + name, function(err, result) {
+  var client = new pg.Client(conString);
+  client.connect(function(err) {
+    if(err) {
+      return console.error('could not connect to postgres', err);
+    }
+    client.query('SELECT * from administrator_info', function(err, result) {
+      if(err) {
+        return console.error('error running query', err);
+      }
+      console.log(result.rows[0]);
+      client.end();
+    });
+  }); 
+      client.query('SELECT * from ' + name, function(err, result) {
     if(err) {
       return console.error('error running query', err);
     }
-    console.log(result.rows[0]);
+    // console.log(result.rows[0]);
     client.end();
   });
 }
