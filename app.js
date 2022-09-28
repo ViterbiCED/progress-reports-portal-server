394b8265a06e77b7c919f88965675816d2933eb8
Ayushi Mittal <ayushimi@usc.edu>
2022-09-28 13:51:46 -0700

    sample add function

--- app.js
+++ app.js
@@ -8,11 +8,20 @@ var conString = "postgres://postgres:401db@localhost:5432/401_db";
 async function select_table(name) {
   var client = new pg.Client(conString);
   await client.connect();
-  const result = await client.query('SELECT * from ' + name);
-  console.log(result.rows[0]);
+  const result = await client.query(`SELECT * from ${name};`);
+  console.log(result.rows);
   client.end();
-  return result.rows[0];
+  return result.rows;
 };
+
+async function add_mentor(name, usc_id, email, phone_number, major) {
+  var client = new pg.Client(conString);
+  await client.connect();
+  await client.query(`INSERT INTO mentor_info (name, usc_id, email, phone_number, major)
+                      VALUES ('${name}', '${usc_id}', '${email}', '${phone_number}', '${major}');`);
+  client.end();
+};
+
 
 app.get('/', function (req, res) {
   res.send('Hello World!');
@@ -23,7 +32,15 @@ app.listen(process.env.PORT || 3000, function () {
 
 app.get('/select', async function (req, res) {
   var table_name = req.query.table_name;
-  var result = await select_table("administrator_info");
+  var result = await select_table(table_name);
+  res.send(result);
+});
+
+app.get('/add_mentor', async function (req, res) {
+  await add_mentor(req.query.name, req.query.usc_id, 
+    req.query.email, req.query.phone_number, 
+    req.query.major);
+  var result = await select_table("mentor_info");
   res.send(result);
 });
 
