f201cec8f37d1aa65cf81880556c97a41bab2842
emdg05 <ed_139@usc.edu>
2022-09-27 23:07:05 -0700

    db function

--- db.js
+++ db.js
@@ -1,12 +1,3 @@
-var express = require('express');
-var app = express();
-app.get('/', function (req, res) {
-  res.send('Hello World!');
-});
-app.listen(process.env.PORT || 3000, function () {
-  console.log('Example app listening on port 3000!');
-});
-
 var pg = require('pg');
 
 var conString = "postgres://postgres:401db@localhost:5432/401_db";
@@ -24,3 +15,13 @@ client.connect(function(err) {
     client.end();
   });
 }); 
+
+function select_table(name) {
+    client.query('SELECT * from ' + name, function(err, result) {
+    if(err) {
+      return console.error('error running query', err);
+    }
+    console.log(result.rows[0]);
+    client.end();
+  });
+}
