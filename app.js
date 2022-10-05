d974ecea6242478b94d52abea78e36cac88baf88
Ayushi Mittal <ayushimi@usc.edu>
2022-10-05 00:32:46 -0700

    tryna fix

--- app.js
+++ app.js
@@ -242,6 +242,7 @@ async function get_user_info(id, role) {
 }
 
 async function search_users_of_table(role, column_name, search_term) {
+  console.log(`SELECT id FROM ${role}_info WHERE ${column_name} LIKE '%${search_term}%';`)
   var result = await client.query(`SELECT id FROM ${role}_info
                     WHERE ${column_name} LIKE '%${search_term}%';`);
   return result.rows;
