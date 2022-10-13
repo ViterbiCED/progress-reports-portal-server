88db2c9301d5f397422ec1ee2e86e8252fe3aa2e
emdg05 <ed_139@usc.edu>
2022-10-12 19:42:42 -0700

    reports api

--- app.js
+++ app.js
@@ -228,22 +228,30 @@ async function get_mentees_of_mentor_id(id) {
   return result.rows;
 }
 
-async function add_progress_report(name, mentor_id, mentee_id, session_date, summary, smart_goal, academic_development, career_development, personal_development, additional_info, session_length, seeking_supervision) {
-  await client.query(`INSERT INTO progress_reports(name, mentor_id, mentee_id, session_date, summary, smart_goal, academic_development, career_development, personal_development, additional_info, session_length, seeking_supervision)
-                      VALUES ('${name}', '${mentor_id}', '${mentee_id}', '${session_date}', '${summary}', '${smart_goal}', '${academic_development}', '${career_development}', '${personal_development}', '${additional_info}', '${session_length}', '${seeking_supervision}');`);
+async function add_progress_report(name, mentor_id, mentee_id, session_date) {
+  await client.query(`INSERT INTO progress_reports(name, mentor_id, mentee_id, session_date)
+                      VALUES ('${name}', '${mentor_id}', '${mentee_id}', '${session_date}');`);
 };
 
 async function find_progress_reports_by_name(mentor_name, mentee_name) {
-  var result = await client.query(`SELECT progress_reports.name, progress_reports.session_date, progress_reports.summary FROM progress_reports, mentor_info, mentee_info
+  var result = await client.query(`SELECT progress_reports.name, progress_reports.session_date, progress_reports.approved FROM progress_reports, mentor_info, mentee_info
                     WHERE mentor_info.name =  '${mentor_name}' AND progress_reports.mentor_id = mentor_info.id AND mentee_info.name =  '${mentee_name}' AND progress_reports.mentee_id = mentee_info.id;`);
   return result.rows;
 };
 
 async function find_progress_reports_by_id(mentor_id, mentee_id) {
-  var result = await client.query(`SELECT progress_reports.name, progress_reports.session_date, progress_reports.summary FROM progress_reports
+  var result = await client.query(`SELECT progress_reports.name, progress_reports.session_date, progress_reports.approved FROM progress_reports
                     WHERE progress_reports.mentor_id = ${mentor_id} AND progress_reports.mentee_id = ${mentee_id};`);
   return result.rows;
 };
+
+async function approve_progress_report(id) {
+  await client.query(`UPDATE reports SET approved = TRUE WHERE id = ${id};`);
+}
+
+async function add_feedback(id, feedback) {
+  await client.query(`UPDATE reports SET feedback = ${feedback} WHERE id = ${id};`);
+}
 
 async function check_value_exists(table_name, column_name, value) {
   var result = await client.query(`SELECT EXISTS(SELECT 1 FROM ${table_name} WHERE ${column_name} = '${value}');`);
@@ -315,6 +323,7 @@ async function get_mentor_of_mentee_id(id) {
   var result = await client.query(`SELECT mentor_id FROM mentors_mentees WHERE mentee_id = ${id};`);
   return result.rows[0];
 }
+
 
 
 
@@ -434,13 +443,13 @@ app.get('/get_mentees_of_mentor_id', async function (req, res) {
 })
 
 /*
-  http://localhost:3000/add_progress_report?name=ProgReport&mentor_id=1&mentee_id=1&session_date=2012-08-01&summary=SUMMARY&smart_goal=SMART_GOAL&academic_development=ACADEMIC_DEVELOPMENT&career_development=CAREER_DEVELOPMENT&personal_development=PERSONAL_DEVELOPMENT&additional_info=ADDITIONAL_INFO&session_length=45&seeking_supervision=true
+  http://localhost:3000/add_progress_report?name=ProgReport&mentor_id=1&mentee_id=1&session_date=2012-08-01
 */
 app.get('/add_progress_report', async function (req, res) {
   var result = null;
-  if (check_query_params(req.query, ["name", "mentor_id", "mentee_id", "session_date", "summary", "smart_goal", "academic_development", "career_development", "personal_development", "additional_info", "session_length", "seeking_supervision"])) {
-    await add_progress_report(req.query.name, req.query.mentor_id, req.query.mentee_id, req.query.session_date, req.query.summary, req.query.smart_goal, req.query.academic_development, req.query.career_development, req.query.personal_development, req.query.additional_info, req.query.session_length, req.query.seeking_supervision);
-    result = await select_table("progress_reports");
+  if (check_query_params(req.query, ["name", "mentor_id", "mentee_id", "session_date"])) {
+    await add_progress_report(req.query.name, req.query.mentor_id, req.query.mentee_id, req.query.session_date);
+    result = await select_table("reports");
   }
   send_res(res, result);
 });
@@ -463,6 +472,28 @@ app.get('/find_progress_reports_by_id', async function (req, res) {
   var result = null;
   if (check_query_params(req.query, ["mentor_id", "mentee_id"])) {
     result = await find_progress_reports_by_id(req.query.mentor_id, req.query.mentee_id);
+  }
+  send_res(res, result);
+});
+
+/*
+  http://localhost:3000/approve_report?id=1
+*/
+app.get('/approve_report', async function (req,res) {
+  var result = null;
+  if (check_query_params(req.query, ["id"])) {
+    result = await approve_progress_report(req.query.id);
+  }
+  send_res(res, result);
+});
+
+/*
+  http://localhost:3000/add_feedback?id=1&feedback=Approved!
+*/
+app.get('/add_feedback', async function (req,res) {
+  var result = null;
+  if (check_query_params(req.query, ["id", "feedback"])) {
+    result = await add_feedback(req.query.id, req.query.feedback);
   }
   send_res(res, result);
 });
