a45269ae2428e3b097b51d36a303152c48ba4969
Uma Durairaj <uduraira@usc.edu>
2022-10-16 01:35:08 -0700

    Add error checking for get_user_info

--- app.js
+++ app.js
@@ -313,7 +313,11 @@ async function get_user_roles(email) {
 
 async function get_user_info(id, role) {
   var result = await client.query(`SELECT * FROM ${role}_info WHERE id = ${id};`);
-  return result.rows;
+  if result.rows.length > 0 {
+    return result.rows[0];
+  } else {
+    return result.rows;
+  }
 }
 
 async function search_users_of_table(role, column_name, search_term) {
